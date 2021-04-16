e6ace9b9532de5f119d066179e34752c428cec10
Peedroca <phm1080@gmail.com>
2021-04-15 21:22:35 -0300

    Implementado singleton

---
+++ source/design-patterns-II/Peedroca.DesignPatternsII/DesignPattersII.Exercise/Models/Servico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersII.Exercise.Models
+{
+    public class Servico
+    {
+
+    }
+}
---
+++ source/design-patterns-II/Peedroca.DesignPatternsII/DesignPattersII.Exercise/Models/ServicoSingleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersII.Exercise.Models
+{
+    public class ServicoSingleton
+    {
+        private static Servico servico = new Servico();
+
+        public Servico Instance { get { return servico; } }
+    }
+}
